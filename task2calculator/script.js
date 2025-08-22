8296ba03d3f6e7b56f45f2e3027637d6fdeb43fd
naveenstheninjastudio <naveens@theninjastudio.com>
2025-08-22 20:53:55 +0530

    onscreen click event add

--- task2calculator/script.js
+++ task2calculator/script.js
@@ -1,9 +1,23 @@
-const btn = document.querySelectorAll(".keypad");
-console.log(btn);
+const btns = document.querySelectorAll(".btn");
 let displayBox = document.querySelector(".display");
 display = () => {
   displayBox.innerText = btn.innerText;
 };
+for (let i = 0; i < btns.length; i++) {
+  let button = btns[i];
+  button.addEventListener("click", (e) => {
+    if (button.innerText === "AC") {
+      displayBox.innerText = "";
+    } else if (button.innerText === "CLR") {
+      displayBox.innerText = displayBox.innerText.slice(0, -1);
+    } else if (button.innerText === "=") {
+      displayBox.innerText = eval(displayBox.innerText);
+    } else {
+      displayBox.innerText += button.innerText;
+    }
+  });
+}
+
 document.addEventListener("keydown", (Event) => {
   const calKeys = [
     "0",
@@ -25,9 +39,9 @@ document.addEventListener("keydown", (Event) => {
   ];
   if (calKeys.includes(Event.key)) {
     displayBox.innerText += Event.key;
-  } else if (Event.key === "=" || Event.key === "Enter") {
-    displayBox.innerText = eval(displayBox.innerText);
   } else if (Event.key === "Backspace") {
     displayBox.innerText = displayBox.innerText.slice(0, -1);
+  } else if (Event.key === "=" || Event.key === "Enter") {
+    displayBox.innerText = eval(displayBox.innerText);
   }
 });
