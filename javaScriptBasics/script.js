f181fa1e100f0d939c07b2a64cc9afc895f87c5c
naveenstheninjastudio <naveens@theninjastudio.com>
2025-08-27 16:20:11 +0530

    task 3 completed

--- javaScriptBasics/script.js
+++ javaScriptBasics/script.js
@@ -5,6 +5,7 @@ console.log(`My name is ${firstName} and I'm ${age} years old`);
 console.log(isStudent);
 const favouriteFruits = ["Banana", "Apple", "Mango"];
 console.log(favouriteFruits);
+
 const person = {
   firstName: "Jennie",
   age: 28,
@@ -12,17 +13,39 @@ const person = {
 };
 console.log(person);
 
-let calcAverage = (r1, r2, r3) => (r1 + r2 + r3) / 3;
-const scoreDolphins = calcAverage(44, 23, 71);
-const scoreKoalas = calcAverage(65, 54, 49);
-
-function checkWinner(avgDolphins, avgKoalas) {
-  if (avgDolphins >= 2 * avgKoalas) {
-    console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
-  } else if (avgKoalas >= 2 * avgDolphins) {
-    console.log(`Koalas win (${avgKoalas} vs. ${avgDolphins})`);
-  } else {
-    console.log(`No team wins...`);
-  }
+function greet(name) {
+  return `Hello ${name}`;
 }
-checkWinner(scoreDolphins, scoreKoalas);
+console.log(greet("Lisa"));
+
+const greet1 = (yourName) => `Hello ${yourName}`;
+console.log(greet1("Rose"));
+
+function addition(a, b) {
+  return a + b;
+}
+
+console.log(addition(98765, 12345));
+
+// task 3
+const car = {
+  brand: "BMW",
+  model: "M4",
+  year: "2025",
+};
+console.log(car);
+car.color = "black";
+console.log(car);
+
+const numbers = [1, 2, 3, 4, 5];
+const numbersX2 = numbers.map((x) => x * 2);
+console.log(numbersX2);
+const evenNumbers = numbers.filter(even);
+function even(e) {
+  return e % 2 === 0;
+}
+console.log(evenNumbers);
+numbers.forEach(print);
+function print(e) {
+  return console.log(e);
+}
